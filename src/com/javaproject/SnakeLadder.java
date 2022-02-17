f04000a594d6244b7c1f4d13593e66d35d110eba
rahulbhamre1997 <rahul1997bhamre@gmail.com>
2022-02-17 09:19:56 +0530

    Added:Checking players position

--- src/com/javaproject/SnakeLadder.java
+++ src/com/javaproject/SnakeLadder.java
@@ -1,7 +1,10 @@
 package com.javaproject;
 
 public class SnakeLadder {
+	
 	public static void main(String[] args) {
-		System.out.println("Welcome to Snake and Ladder");
+		
+		int playersposition=0;
 	}
+
 }
