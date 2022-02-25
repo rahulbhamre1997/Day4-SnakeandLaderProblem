0e5757700e0499abbad051600c8db8b54191ae97
rahulbhamre1997 <rahul1997bhamre@gmail.com>
2022-02-25 23:20:46 +0530

    Added: count after every die

--- src/com/javaproject/SnakeLadder.java
+++ src/com/javaproject/SnakeLadder.java
@@ -4,43 +4,46 @@ import java.util.Random;
 
 public class SnakeLadder {
 	
-		static final int noPlay = 0;
-		static final int ladder = 1;
-		static final int snake = 2;
+	static final int noPlay = 0;
+	static final int ladder = 1;
+	static final int snake = 2;
+	static int count = 0;
+	
+	public static void main(String[] args) {
+		int position = 0;
 		
-			public static void main(String[] args) {
-			int position = 0;
-			
 			while (position < 100) {
 				Random ran = new Random();
-		        int dice = ran.nextInt(6)+1;
-		        
+				int dice = ran.nextInt(6)+1;
+				++count; 
+        
 				System.out.println("dice: " + dice);
-		        int optionCheck = ran.nextInt(3);
+				int optionCheck = ran.nextInt(3);
 				System.out.println("optionCheck: " + optionCheck);
-				
+		
 				if ((optionCheck == ladder) && (position + dice) <= 100) 
 				{
 					System.out.println("Ladder");
 					position = position + dice;
-				}
-				
+				}	
+		
 				else if (optionCheck == snake) 
 				{
 					System.out.println("Snake");
 					position = position - dice;
 				}
-				
+		
 				else
 				{
 					System.out.println("No Play");
 				}
-				
+		
 				if (position < 0)
-					position = 0;
-				
-				System.out.println("position: " + position);
+				position = 0;
+		
+				System.out.println("position: " + position+ "\nDice count" + count);
 				}       
-		        
-			}
+        
 		}
+}
+
