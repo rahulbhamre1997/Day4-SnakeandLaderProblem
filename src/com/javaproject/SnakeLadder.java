fe24be8e225d5c229fee41d2a672ab311744eb2e
rahulbhamre1997 <rahul1997bhamre@gmail.com>
2022-02-25 15:02:20 +0530

    Added:players repeat positin till 100

--- src/com/javaproject/SnakeLadder.java
+++ src/com/javaproject/SnakeLadder.java
@@ -8,33 +8,34 @@ public class SnakeLadder {
 		static final int ladder = 1;
 		static final int snake = 2;
 		
-		
 		public static void main(String[] args) {
 			int position = 0;
 			
-			Random ran = new Random();
-	        int dice = ran.nextInt(6)+1;
-			System.out.println("dice: "+dice);
-	        int optionCheck = ran.nextInt(3);
-			System.out.println("optionCheck: "+optionCheck);
-			
-			if (optionCheck == ladder) 
-			{
-				System.out.println("Ladder");
-				position = position + dice;
+			while (position < 100) {
+				Random ran = new Random();
+		        int dice = ran.nextInt(6)+1;
+		        
+				System.out.println("dice: " + dice);
+		        int optionCheck = ran.nextInt(3);
+				System.out.println("optionCheck: " + optionCheck);
+				
+				if ((optionCheck == ladder) && (position + dice) <= 100) 
+				{
+					System.out.println("Ladder");
+					position = position + dice;
+				}
+				
+				else if (optionCheck == snake) 
+				{
+					System.out.println("Snake");
+					position = position - dice;
+				}
+				else {
+					System.out.println("No Play");
+				}
+				if (position < 0)
+					position = 0;
+				System.out.println("position: " + position);
+				}         
 			}
-			
-			else if (optionCheck == snake) 
-			{
-				System.out.println("Snake");
-				position = position - dice;
-			}
-			else {
-				System.out.println("No Play");
-			}
-			if (position<0)
-				position=0;
-			System.out.println("position: "+position);
 		}
-
-}
